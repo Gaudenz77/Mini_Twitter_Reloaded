339ffc69b18dfc71972e9e3b9ce1e9568e682e23
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-06-01 13:08:13 +0200

    prepared for run build

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/app.css',
+                'resources/css/areset.css', 
+                'resources/css/custom.css',
+                'resources/js/custom.js',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
